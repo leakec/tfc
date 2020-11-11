8f67c7d8f605e1f683195b8f3340156faec7fceb
leakec <leakec57@gmail.com>
2020-11-11 11:03:08 -0600

    * Updated step test, as step is now in tfc.utils.

--- tests/test_step.py
+++ tests/test_step.py
@@ -2,29 +2,25 @@ import jax.numpy as np
 
 from tfc import utfc as TFC
 from tfc import mtfc as nTFC
-from tfc.utils import egrad
+from tfc.utils import egrad, step
 
 def test_step():
     A = np.array([5.,4.,-1.,1.,0.])
-    tfc = TFC(10,0,3,basis='CP',x0=-1.,xf=1.)
-    ntfc = nTFC(np.array([5,5]),np.array([0,0]),3,dim=2,basis='CP',x0=[-1.,-1.],xf=[1.,1.])
-    B = tfc.step(A)
-    C = ntfc.step(A)
+    B = step(A)
+    C = step(A)
     assert(np.all(B == np.array([1.,1.,0.,1.,0.])))
     assert(np.all(B == C))
 
     A = np.array([[5.,4.,-1.,1.,0.],[-1.5,0.,0.,2.2,5.6]])
-    B = tfc.step(A)
-    C = ntfc.step(A)
+    B = step(A)
+    C = step(A)
     assert(np.all(B == np.array([[1.,1.,0.,1.,0.],[0.,0.,0.,1.,1.]])))
     assert(np.all(B == C))
 
 def test_dstep():
     A = np.array([5.,4.,-1.,1.,0.])
-    tfc = TFC(10,0,3,basis='CP',x0=-1.,xf=1.)
-    ntfc = nTFC(np.array([5,5]),np.array([0,0]),3,dim=2,basis='CP',x0=[-1.,-1.],xf=[1.,1.])
-    B = lambda A: tfc.step(A)
-    C = lambda A: ntfc.step(A)
+    B = lambda A: step(A)
+    C = lambda A: step(A)
     D = egrad(B,0)(A)
     E = egrad(C,0)(A)
     assert(np.all(D == np.zeros_like(D)))
@@ -36,16 +32,16 @@ def test_dstep():
     assert(np.all(D == np.zeros_like(D)))
     assert(np.all(E == D))
 
-    B = lambda A: A*tfc.step(A)+A
-    C = lambda A: A*ntfc.step(A)+A
+    B = lambda A: A*step(A)+A
+    C = lambda A: A*step(A)+A
     D = egrad(B,0)(A)
     E = egrad(C,0)(A)
     true = np.array([[1.,1.,0.,1.,0.],[0.,0.,0.,1.,1.]])+np.ones_like(A)
     assert(np.all(D == true))
     assert(np.all(E == D))
 
-    B = lambda A: A**3*tfc.step(A)+np.sin(A)*tfc.step(A)
-    C = lambda A: A**3*ntfc.step(A)+np.sin(A)*ntfc.step(A)
+    B = lambda A: A**3*step(A)+np.sin(A)*step(A)
+    C = lambda A: A**3*step(A)+np.sin(A)*step(A)
     D = egrad(egrad(B,0),0)(A)
     E = egrad(egrad(C,0),0)(A)
     true = 6.*A*np.array([[1.,1.,0.,1.,0.],[0.,0.,0.,1.,1.]])-np.sin(A)*np.array([[1.,1.,0.,1.,0.],[0.,0.,0.,1.,1.]])
