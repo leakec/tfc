e0dfacb787a2adb2e9e76812423d7f54ddd74366
leakec <leakec57@gmail.com>
2020-11-18 16:27:03 -0600

    * Made functions user does not need private.

--- src/tfc/utils/Latex.py
+++ src/tfc/utils/Latex.py
@@ -4,18 +4,18 @@ class table:
     """ This class is used to create the text needed for latex tables. """
 
     @staticmethod
-    def Header(numCols):
+    def _Header(numCols):
         """ This function creates the table header based on the number of columns. """
         return '\\begin{center}\n\\begin{tabular}{'+'|c'*numCols+'|}\n'
 
     @staticmethod
-    def colHeader(strIn):
+    def _colHeader(strIn):
         """ This function creates the column header based on the list of strings that are passed in
             via the input strIn. """
         return ' & '.join(strIn)+'\\\\\n'
 
     @staticmethod
-    def Arr2Tab(arrIn,form='%.4E',rowHeader=None):
+    def _Arr2Tab(arrIn,form='%.4E',rowHeader=None):
         """ This function transforms the 2-D numpy array (arrIn) into latex tabular format. The 
             "form" argument specifies the number format to be used in the tabular environment. 
             The "rowHeader" argument is a list of strings that are used in the first column of
@@ -52,7 +52,7 @@ class table:
         return out.rstrip()
 
     @staticmethod
-    def Footer():
+    def _Footer():
         """ This function creates the footer for the latex table. """
         return '\\end{tabular}\n\\end{center}'
 
@@ -66,13 +66,13 @@ class table:
             environment is returned as a string."""
 
         if colHeader is None and rowHeader is None:
-            return table.Header(arrIn.shape[1])+'\\hline\n'+table.Arr2Tab(arrIn,form=form)+'\n'+table.Footer()
+            return table._Header(arrIn.shape[1])+'\\hline\n'+table._Arr2Tab(arrIn,form=form)+'\n'+table._Footer()
         elif rowHeader is None:
-            return table.Header(arrIn.shape[1])+'\\hline\n'+table.colHeader(colHeader)+'\\hline\n'+table.Arr2Tab(arrIn,form=form)+'\n'+table.Footer()
+            return table._Header(arrIn.shape[1])+'\\hline\n'+table._colHeader(colHeader)+'\\hline\n'+table._Arr2Tab(arrIn,form=form)+'\n'+table._Footer()
         elif colHeader is None:
-            return table.Header(arrIn.shape[1]+1)+'\\hline\n'+table.Arr2Tab(arrIn,form=form,rowHeader=rowHeader)+'\n'+table.Footer()
+            return table._Header(arrIn.shape[1]+1)+'\\hline\n'+table._Arr2Tab(arrIn,form=form,rowHeader=rowHeader)+'\n'+table._Footer()
         else:
-            return table.Header(arrIn.shape[1]+1)+'\\hline\n'+table.colHeader(colHeader)+'\\hline\n'+table.Arr2Tab(arrIn,form=form,rowHeader=rowHeader)+'\n'+table.Footer()
+            return table._Header(arrIn.shape[1]+1)+'\\hline\n'+table._colHeader(colHeader)+'\\hline\n'+table._Arr2Tab(arrIn,form=form,rowHeader=rowHeader)+'\n'+table._Footer()
 
 
 
