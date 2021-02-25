3d8ee9c0dcd26ef809ec0aca20ab73e91f27c91f
leakec <leakec57@gmail.com>
2021-02-25 07:56:26 -0800

    * Incrementing version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cmaketools import setup
 
 setup(
     name="tfc",
-    version="0.0.8",
+    version="0.0.9",
     author="Carl Leake and Hunter Johnston",
     author_email="leakec57@gmail.com",
     description="Theory of Functional Connections (TFC): A functional interpolation framework with applications in differential equations.",
