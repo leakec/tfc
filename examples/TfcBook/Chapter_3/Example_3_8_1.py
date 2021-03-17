20bda077d87748d169ef6e69cafc8fc7bc3c1bf1
hunterjohnston <hunter.r.johnston@gmail.com>
2021-03-17 15:23:13 -0500

    checked all plotting

--- examples/TfcBook/Chapter_3/Example_3_8_1.py
+++ examples/TfcBook/Chapter_3/Example_3_8_1.py
@@ -7,16 +7,18 @@
 #   subject to: y(0) = 1, y'(0) = 0
 ####################################################################################################
 from tfc import utfc
-from tfc.utils import egrad, NLLS, LS
+from tfc.utils import egrad, NLLS, LS, MakePlot
 import jax.numpy as np
+
+import numpy as onp
 ####################################################################################################
 
 ## user defined parameters: ************************************************************************
 N = 100 # number of discretization points
 m = 60  # number of basis function terms
 basis = 'CP' # basis function type
-a = 1   # specific problem type, a >=0 (analytical solution known for a = 0, 1, and 5)
-xspan = np.array([0., 10.]) # problem domain range [x0, xf], where x0 > 0
+a = 0   # specific problem type, a >=0 (analytical solution known for a = 0, 1, and 5)
+xspan = [0., 10.] # problem domain range [x0, xf], where x0 > 0
 
 ## problem initial conditions: *********************************************************************
 y0  = 1.  # y(x0)  = 1
@@ -44,7 +46,7 @@ ypp = egrad(yp)
 L = lambda xi: x*ypp(x,xi) + 2.*yp(x,xi) + x*y(x,xi)**a
 
 ## solve the problem via nonlinear least-squares ***************************************************
-xi = np.zeros(H.shape[1])
+xi = np.zeros(H(x).shape[1])
 
 # if a==0 or a == 1, the problem is linear
 if a == 0 or a == 1:
@@ -58,10 +60,10 @@ else:
 if a == 0:
     ytrue = 1. - 1./6. * x**2
 elif a == 1:
-    ytrue = np.ones_like(x)
+    ytrue = onp.ones_like(x)
     ytrue[1:] = np.sin(x[1:]) / x[1:]
 elif a == 5:
-    ytrue = 1. + x**2/3)**(-1/2)
+    ytrue = (1. + x**2/3)**(-1/2)
 else:
     ytrue = np.empty_like(x)
 
@@ -81,15 +83,17 @@ p1.show()
 
 # figure 2: residual
 p2 = MakePlot(r'$x$',r'$|L(\xi)|$')
-p2.ax[0].plot(x,res)
+p2.ax[0].plot(x,res,'*')
 p2.ax[0].grid(True)
+p2.ax[0].set_yscale('log')
 p2.PartScreen(7.,6.)
 p2.show()
 
 # figure 3: error (if a = 0, 1, or 5)
 if a == 0 or a == 1 or a == 5:
     p3 = MakePlot(r'$x$',r'$|y_{true} - y(x)|$')
-    p3.ax[0].plot(x,err)
+    p3.ax[0].plot(x,err,'*')
     p3.ax[0].grid(True)
+    p3.ax[0].set_yscale('log')
     p3.PartScreen(7.,6.)
     p3.show()
