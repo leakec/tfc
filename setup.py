8ae5dc6069e34259f363a34533153e64cf99c2eb
leakec <leakec57@gmail.com>
2021-03-25 16:10:07 -0700

    * Updated version number and added long description so it will show on PyPI.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from cmaketools import setup
 
+# read the contents your README
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="tfc",
-    version="0.0.9",
+    version="0.0.10",
     author="Carl Leake and Hunter Johnston",
     author_email="leakec57@gmail.com",
     description="Theory of Functional Connections (TFC): A functional interpolation framework with applications in differential equations.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/leakec/tfc.git",
     license="MIT",
     classifiers = ["Development Status :: 4 - Beta",
