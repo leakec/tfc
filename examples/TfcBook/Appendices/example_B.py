a86447c4128f4f2d0f50c28ab3bd94d8c5dbd5a7
hunterjohnston <hunter.r.johnston@gmail.com>
2021-04-15 10:27:28 -0500

    Fixing a minor initialization issue

---
+++ examples/TfcBook/Appendices/example_B.py
@@ -0,0 +1,90 @@
+# This script solves the simple boundary-value problem with spectral method (Appendix B) 
+# in the TFC book
+####################################################################################################
+# Differential Equation
+#   y'' + yy' = exp(-2x) sin(x) [cos(x) -sin(x)] - 2exp(-x)cos(x)
+#
+#   subject to: y(0) = 0, y(pi) = 0
+####################################################################################################
+from tfc import utfc
+from tfc.utils import egrad, NLLS, LS, MakePlot
+import jax.numpy as np
+
+import numpy as onp
+####################################################################################################
+
+## user defined parameters: ************************************************************************
+N = 100         # number of discretization points
+m = 60          # number of basis function terms
+basis = 'CP'    # basis function type
+
+## problem boundary conditions: ********************************************************************
+xspan = [0., np.pi]
+y0  = 0.    # y(0)  = 0
+yf  = 0.    # y(pi) = 0
+nC  = 0     # number of constraints, we are solvoing this with spectral method
+
+## construct univariate tfc class: *****************************************************************
+tfc = utfc(N, nC, int(m), basis = basis, x0=xspan[0], xf=xspan[1])
+x = tfc.x
+H = tfc.H
+
+## define the candidate solution: ******************************************************************
+
+# forcing term
+f = lambda x: np.exp(-2.*x) * np.sin(x) * (np.cos(x) - np.sin(x)) - 2.*np.exp(-x)*np.cos(x)
+
+# tfc constrained expression
+y = lambda x,xi: np.dot(H(x),xi)
+yp = egrad(y)
+ypp = egrad(yp)
+
+## define the loss function: ***********************************************************************
+L0      = lambda xi: y(x,xi)[0] - y0
+Lf      = lambda xi: y(x,xi)[-1] - yf
+Ldif    = lambda xi: ypp(x,xi) + y(x,xi)*yp(x,xi) - f(x)
+
+L = lambda xi: np.hstack(( L0(xi), Ldif(xi)[1:-1], Lf(xi) ))
+
+## solve the problem via nonlinear least-squares ***************************************************
+xi = onp.zeros(H(x).shape[1])
+# xi[0] =  1.29615141e-01
+# xi[1] = -8.82056560e-02
+
+xi,iter,time = NLLS(xi,L,timer=True)
+
+## compute the error: ******************************************************************************
+ytrue = np.exp(-x) * np.sin(x)
+
+err = np.abs(y(x,xi) - ytrue)
+
+## compute the residual of the loss vector: ********************************************************
+res = np.abs(L(xi))
+
+## plotting: ***************************************************************************************
+
+# figure 1: solution
+p1 = MakePlot(r'$x$',r'$y(x)$')
+p1.ax[0].plot(x,y(x,xi))
+p1.ax[0].grid(True)
+p1.ax[0].set_xlim(xspan[0], xspan[1])
+p1.PartScreen(7.,6.)
+p1.show()
+
+# figure 2: residual
+p2 = MakePlot(r'$x$',r'$|L(\xi)|$')
+p2.ax[0].plot(x,res,'*')
+p2.ax[0].grid(True)
+p2.ax[0].set_yscale('log')
+p2.ax[0].set_xlim(xspan[0], xspan[1])
+p2.PartScreen(7.,6.)
+p2.show()
+
+# figure 3: error
+p3 = MakePlot(r'$x$',r'$|y_{true} - y(x)|$')
+p3.ax[0].plot(x,err,'*')
+p3.ax[0].grid(True)
+p3.ax[0].set_yscale('log')
+p3.ax[0].set_xlim(xspan[0], xspan[1])
+p3.PartScreen(7.,6.)
+p3.show()
