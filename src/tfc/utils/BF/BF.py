62bb27f6490e853c42d312546a4e445c9bc18dc1
leakec <leakec57@gmail.com>
2021-04-28 14:23:03 -0700

    * Reformatted with black.

--- src/tfc/utils/BF/BF.py
+++ src/tfc/utils/BF/BF.py
@@ -1,40 +1,77 @@
 """ This is a dummy file used only to avoid errors in ReadTheDocs. The real BF.py is created during the setup once swig is run. """
+
+
 def CP():
     pass
+
+
 def LeP():
     pass
+
+
 def LaP():
     pass
+
+
 def HoPpro():
     pass
+
+
 def HoPphy():
     pass
+
+
 def FS():
     pass
+
+
 def ELMReLU():
     pass
+
+
 def ELMSigmoid():
     pass
+
+
 def ELMTanh():
     pass
+
+
 def ELMSin():
     pass
+
+
 def ELMSwish():
     pass
 
+
 def nCP():
     pass
+
+
 def nLeP():
     pass
+
+
 def nFS():
     pass
+
+
 def nELMReLU():
     pass
+
+
 def nELMSigmoid():
     pass
+
+
 def nELMTanh():
     pass
+
+
 def nELMSin():
     pass
+
+
 def nELMSwish():
     pass
