eb889851435416f4dc4890f59abaacfac16e6cc5
leakec <leakec57@gmail.com>
2022-08-27 08:26:59 -0700

    Adding types file that takes care of Python version control. Will also be useful for #9.

---
+++ src/tfc/utils/types.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Union
+if sys.version_info >= (3,9):
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
+from annotated_types import Gt
+
+uint = Annotated[int, Gt(0)]
+Number = Union[int, float, complex]
