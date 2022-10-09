24b065a6088a1428ff6d2ebb731e761f9fe6cfca
leakec <leakec57@gmail.com>
2022-10-08 17:48:43 -0700

    Adding version info via a version file. * Increasing required numpy version since we need up-to-date type info.

---
+++ src/tfc/version.py
@@ -0,0 +1,12 @@
+import sys
+if sys.version_info >= (3, 9):
+    Tuple = tuple
+else:
+    from typing import Tuple
+
+__version__ = "0.1.11"
+
+def _version_as_tuple(version_str: str) -> Tuple[int, ...]:
+  return tuple(int(i) for i in version_str.split(".") if i.isdigit())
+
+__version_info__ = _version_as_tuple(__version__)
