548c7786b820c7affac0607de1723c73100bd8cd
leakec <leakec57@gmail.com>
2024-09-14 17:08:38 -0700

    Removing Python 3.9, since jaxlib doesn't support it anymore. * Trying to build jaxlib for MacOS from source in the GitHub action to   fix AVX instruction issue.

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"": ["src/tfc/py.typed"]},
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     include_package_data=True,
     ext_modules=[BF],
     install_requires=[
