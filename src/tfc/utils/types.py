deb9f4993729ba1cba53a85eb5434a5baf59aad4
leakec <leakec57@gmail.com>
2025-08-02 08:06:47 -0700

    Fixing types now that we are on 3.10 or greater.

--- src/tfc/utils/types.py
+++ src/tfc/utils/types.py
@@ -6,21 +6,7 @@ from jax import Array
 from sympy.core.function import AppliedUndef
 from sympy import Expr
 
-if sys.version_info >= (3, 8):
-    from typing import Literal, Protocol, TypedDict
-else:
-    from typing_extensions import Literal, Protocol, TypedDict
-
-if sys.version_info >= (3, 9):
-    from typing import Annotated
-
-    List = list
-    Tuple = tuple
-    Dict = dict
-    Tuple = tuple
-else:
-    from typing_extensions import Annotated
-    from typing import List, Tuple, Dict, Tuple
+from typing import Literal, Protocol, TypedDict, Annotated
 
 from annotated_types import Gt, Ge, Lt, Le
 
@@ -37,26 +23,21 @@ uint = Annotated[int, Ge(0)]
 # General number type
 Number = Union[int, float, complex]
 
-if sys.version_info >= (3, 8):
-    from numpy._typing._array_like import _ArrayLikeStr_co, _ArrayLikeInt_co
+from numpy._typing._array_like import _ArrayLikeStr_co, _ArrayLikeInt_co
 
-    # Array-like of strings
-    StrArrayLike = _ArrayLikeStr_co
+# Array-like of strings
+StrArrayLike = _ArrayLikeStr_co
 
-    # Array-like of integers
-    IntArrayLike = _ArrayLikeInt_co
-else:
-    # Hacks to keep things working for Python 3.7
-    StrArrayLike = Any
-    IntArrayLike = Any
+# Array-like of integers
+IntArrayLike = _ArrayLikeInt_co
 
 # List or array like
-NumberListOrArray = Union[Tuple[Number, ...], List[Number], npt.NDArray[Any], Array]
+NumberListOrArray = Union[tuple[Number, ...], list[Number], npt.NDArray[Any], Array]
 
 # List or array of integers
 IntListOrArray = Union[
-    Tuple[int, ...],
-    List[int],
+    tuple[int, ...],
+    list[int],
     npt.NDArray[np.int32],
     npt.NDArray[np.int64],
     npt.NDArray[np.int16],
@@ -67,14 +48,14 @@ IntListOrArray = Union[
 JaxOrNumpyArray = Union[npt.NDArray, Array]
 
 # Tuple or list of array
-TupleOrListOfArray = Union[Tuple[JaxOrNumpyArray, ...], List[JaxOrNumpyArray]]
-TupleOrListOfNumpyArray = Union[Tuple[npt.NDArray, ...], List[npt.NDArray]]
+TupleOrListOfArray = Union[tuple[JaxOrNumpyArray, ...], list[JaxOrNumpyArray]]
+TupleOrListOfNumpyArray = Union[tuple[npt.NDArray, ...], list[npt.NDArray]]
 
 # Sympy constraint operator
 # Adding in Any here since sympy types are a bit funky at the moment
 ConstraintOperator = Callable[[Union[AppliedUndef, Expr, Any]], Union[AppliedUndef, Any]]
-ConstraintOperators = Union[List[ConstraintOperator], Tuple[ConstraintOperator, ...]]
+ConstraintOperators = Union[list[ConstraintOperator], tuple[ConstraintOperator, ...]]
 
 # List or tuple of sympy expressions
 # Adding in Any here since sympy types are a bit funky at the moment
-Exprs = Union[List[Union[Expr, Any]], Tuple[Union[Expr, Any], ...]]
+Exprs = Union[list[Union[Expr, Any]], tuple[Union[Expr, Any], ...]]
